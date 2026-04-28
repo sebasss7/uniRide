30e35cefc677f4769355b89baf7cbecfa2c0cb99
karenca01 <kcervantes01@alumnos.uaq.mx>
2026-04-28 15:59:44 -0600

    feat: vehicles mock

---
+++ mock/vehicles.ts
@@ -0,0 +1,24 @@
+import { Vehiculo } from '@/types';
+
+export const vehiclesMock: Vehiculo[] = [
+    {
+        id: '1',
+        usuario_id: '1',
+        marca: 'Toyota',
+        modelo: '2024 Corolla Hybrid LE',
+        placas: 'UTC-54-89',
+        color: 'Blanco',
+        licencia: 'QRO-2024-001',
+        imagen_vehiculo: undefined,
+    },
+    {
+        id: '2',
+        usuario_id: '1',
+        marca: 'Toyota',
+        modelo: '2024 Corolla Hybrid LE',
+        placas: 'UTC-54-89',
+        color: 'Gris',
+        licencia: 'QRO-2024-002',
+        imagen_vehiculo: undefined,
+    },
+];
